3c6d188211e2f606c4c2aa0bf13626bf1e1a89e3
Tyler <tylershaynebrown@gmail.com>
2020-11-03 17:14:29 -0500

    Update client-config.js

--- web/client-config.js
+++ web/client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
-    projectId: process.env.SANITY_PROJECT_ID || 'fyup3w96',
+    projectId: process.env.SANITY_PROJECT_ID || 'PASTE-PROJECT-ID-HERE',
     dataset: process.env.SANITY_DATASET || 'production'
   }
 }
